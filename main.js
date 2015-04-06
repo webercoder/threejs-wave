6b855c4b4bcaa0d0fc68d2bf9244140965c0ceb3
Michael Weber <msweber@gmail.com>
2015-04-06 18:54:42 +1000

    Added multiple lines.

--- main.js
+++ main.js
@@ -3,19 +3,54 @@
 	function Wave() {}
 	Wave.prototype = {
 		init: function(world) {
-			this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			this.three_element = new THREE.Mesh(this.geometry, this.material);
+			var line;
+			this.world = world;
+			this.lines = [];
+			this.create_wave();
+			this.add_new_lines();
 			this.register_animation_callbacks(world);
 		},
-		get_three_element: function() {
+		create_wave: function() {
+			var x = 5, y, z;
+			for (z = 0; z < 1000; z++) {
+				y = x * Math.sin(0.1 * z) - x;
+				this.lines.push({
+					added: false,
+					obj: this.create_line(x, y, z)
+				});
+			}
+		},
+		create_line: function(x, y, z) {
+			var geometry = new THREE.Geometry(),
+			material,
+			line;
+			geometry.vertices.push(new THREE.Vector3(-1 * x, y, z));
+			geometry.vertices.push(new THREE.Vector3(x, y, z));
+			geometry.verticesNeedUpdate = true;
+			material = new THREE.MeshBasicMaterial({color: 0xffffff});
+			line = new THREE.Line(geometry, material);
+			line.dynamic = true;
+			return line;
+		},
+		add_new_lines: function() {
+			for (var i = 0; i < this.lines.length; i++) {
+				if (! this.lines[i].added) {
+					this.world.scene.add(this.lines[i].obj);
+					this.lines[i].added = true;
+				}
+			}
 			return this.three_element;
 		},
 		register_animation_callbacks: function(world) {
 			var _this = this;
-			world.register_animation_callback(function callbackRotate(){
-				_this.three_element.rotation.x += 0.01;
-				_this.three_element.rotation.y += 0.01;
+			world.register_animation_callback(function callbackMove(){
+				for (var i = 0; i < _this.lines.length; i++) {
+					var vertices = _this.lines[i].obj.geometry.vertices;
+					for (var j = 0; j < vertices.length; j++) {
+						vertices[j].z -= 0.05;
+					}
+					_this.lines[i].obj.geometry.verticesNeedUpdate = true;
+				}
 			});
 		}
 	}
@@ -47,7 +82,6 @@
 			render();
 		},
 		add: function(obj) {
-			this.scene.add(obj);
 		},
 		move_camera: function(x, y, z) {
 			if (x !== undefined) {
@@ -70,7 +104,6 @@
 		wave = new Wave();
 		world.init();
 		wave.init(world);
-		world.add(wave.get_three_element());
 		world.move_camera(undefined, undefined, 5);
 		world.start();
 	}
